a9562faffaf47fe909044730215b0f8d6a75ccb8
Elvis <Elvisraphael00@gmail.com>
2024-12-02 17:40:15 +0100

    styled new item

--- github/src/components/context/async.tsx
+++ github/src/components/context/async.tsx
@@ -1,7 +1,5 @@
 import axios from "axios"
-// import { createContext, useEffect, useState } from "react"
 
-// const userContext = createContext()
 export const getUsers = async (text: string) => {
   const user = await axios.get(`https://api.github.com/search/users?q=${text}`)
   console.log(user.data)
--- github/src/components/pages/ResultsItem.tsx
+++ github/src/components/pages/ResultsItem.tsx
@@ -5,6 +5,7 @@ function ResultItem() {
         <img src='' alt='' />
       </div>
       <h1>Elvivx</h1>
+      <span className='bio'>This is mine</span>
       <span className='location'>
         <svg height='28' viewBox='0 0 48 48' width='28' xmlns='http://www.w3.org/2000/svg'>
           <path fill='#AE445A' d='M24 4c-7.73 0-14 6.27-14 14 0 10.5 14 26 14 26s14-15.5 14-26c0-7.73-6.27-14-14-14zm0 19c-2.76 0-5-2.24-5-5s2.24-5 5-5 5 2.24 5 5-2.24 5-5 5z' />
