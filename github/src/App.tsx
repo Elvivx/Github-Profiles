f80d0c746d60d9848e43c4e9037b12d5d5dc0b6a
Elvis <Elvisraphael00@gmail.com>
2024-12-02 18:40:20 +0100

    cleaned it up

--- github/src/App.tsx
+++ github/src/App.tsx
@@ -1,6 +1,3 @@
-// import Search from "./components/pages/Search"
-// import Results from "./components/pages/Results"
-// import { Github } from "lucide-react"
 import { GitContextProvider } from "./components/context/async"
 import Home from "./Home"
 import "./assets/scss/App.scss"
@@ -8,9 +5,6 @@ function App() {
   return (
     <GitContextProvider>
       <section className='main'>
-        {/* <Github />
-        <Search />
-        <Results /> */}
         <Home />
       </section>
     </GitContextProvider>
