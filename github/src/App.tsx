63864d921ce3f6547c48cc12928c3be826baef9e
Elvis <Elvisraphael00@gmail.com>
2024-12-02 15:20:46 +0100

    styleed the view

--- github/src/App.tsx
+++ github/src/App.tsx
@@ -1,9 +1,12 @@
 import Search from "./Search"
 import Results from "./Results"
+import { Github } from "lucide-react"
+import "./assets/scss/App.scss"
 function App() {
   return (
     <>
       <section className='main'>
+        <Github />
         <Search />
         <Results />
       </section>
