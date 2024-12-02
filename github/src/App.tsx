e55791990d6cbc515c2ee7ac8e2f9216477fb9c4
Elvis <Elvisraphael00@gmail.com>
2024-12-02 14:14:36 +0100

    Start

---
+++ github/src/App.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+function App(){
+  return(
+    <>
+    <section className="main">
+
+    </section>
+    </>
+  )
+}
+function Search(){
+  return(
+    <>
+    <div className="search"></div>
+    </>
+  )
+}
+function Results(){
+return()
+}
