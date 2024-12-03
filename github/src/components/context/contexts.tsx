8768cbf3fbe1765adff4d5ae6ed4cf8d4af7cc9c
Elvis <Elvisraphael00@gmail.com>
2024-12-03 16:57:31 +0100

    added user state

--- github/src/components/context/contexts.tsx
+++ github/src/components/context/contexts.tsx
@@ -5,6 +5,7 @@ export const GitContext = createContext()
 export const GitContextProvider = ({ children }) => {
   const [text, setText] = useState("")
   const [users, setUsers] = useState("")
+  const [user, setUser] = useState("")
   const [loading, setLoading] = useState(false)
   const getUsers = async () => {
     if (text == "") return
@@ -19,6 +20,7 @@ export const GitContextProvider = ({ children }) => {
 
   return <GitContext.Provider value={{ getUsers, text, setText, users, loading }}>{children}</GitContext.Provider>
 }
+
 const cliente_id = "82d4ed29477f68045158"
 const cliente_secret = "412bc1b12514bd61b5a46df0d6aeddd993701510"
 
@@ -26,4 +28,4 @@ const getUserInfos = async () => {
   let data = await axios.get(`https://api.github.com/users/elvivx?client_id=${cliente_id}&client_secret=${cliente_secret}`)
   console.log(data.data)
 }
-getUserInfos()
+// getUserInfos()
