158e6ecdbf5775388c620b80906d2322b7e3910d
Elvis <Elvisraphael00@gmail.com>
2024-12-03 17:52:54 +0100

    use the get user func

--- github/src/components/pages/ResultsItem.tsx
+++ github/src/components/pages/ResultsItem.tsx
@@ -1,6 +1,9 @@
+import { GitContext } from "../context/contexts"
+import { useContext } from "react"
 function ResultItem({ user }) {
+  const { getUser } = useContext(GitContext)
   return (
-    <div className='result'>
+    <div className='result' onClick={getUser}>
       <div className='img'>
         <img src={user.avatar_url} alt={user.login} />
       </div>
