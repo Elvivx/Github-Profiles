f5b9045b6ab7b487a8f3d9546b315b75e3a0c082
Elvis <Elvisraphael00@gmail.com>
2024-12-03 18:17:06 +0100

    added the axios for repos and starred

--- github/src/components/context/contexts.tsx
+++ github/src/components/context/contexts.tsx
@@ -6,6 +6,9 @@ export const GitContextProvider = ({ children }) => {
   const [text, setText] = useState("")
   const [users, setUsers] = useState("")
   const [user, setUser] = useState("")
+  const [userRepos, setUserRepos] = useState("")
+  const [userStarred, setUserStarred] = useState("")
+  const [userLangs, setUserLangs] = useState("")
   const [loading, setLoading] = useState(false)
   const getUsers = async () => {
     if (text == "") return
@@ -22,11 +25,14 @@ export const GitContextProvider = ({ children }) => {
   const cliente_secret = "412bc1b12514bd61b5a46df0d6aeddd993701510"
 
   const userInfo = async (info) => {
-    const data = await axios.get(`https://api.github.com/users/${info}?client_id=${cliente_id}&client_secret=${cliente_secret}`)
-    console.log(data.data)
-    setUser(data.data)
+    const user = await axios.get(`https://api.github.com/users/${info}?client_id=${cliente_id}&client_secret=${cliente_secret}`)
+    console.log(user.data)
+    setUser(user.data)
 
-    const star = await axios.get(`https://api.github.com/users/${usuario}/repos?per_page=${limite_repositorios}&client_id=${cliente_id}&client_secret=${cliente_secret}`)
+    const repos = await axios.get(`https://api.github.com/users/${info}/repos?per_page=${limite_repositorios}&client_id=${cliente_id}&client_secret=${cliente_secret}`)
+    console
+
+    const stars = await axios.get(`https://api.github.com/users/${info}/starred?per_page=${limite_repositorios}&client_id=${cliente_id}&client_secret=${cliente_secret}`)
   }
 
   const vals = {
