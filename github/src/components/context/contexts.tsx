05a17c1e30d85d53a4a4df1a7c5bc6421d966999
Elvis <Elvisraphael00@gmail.com>
2024-12-03 16:53:29 +0100

    got the auth for more data

--- github/src/components/context/contexts.tsx
+++ github/src/components/context/contexts.tsx
@@ -19,3 +19,11 @@ export const GitContextProvider = ({ children }) => {
 
   return <GitContext.Provider value={{ getUsers, text, setText, users, loading }}>{children}</GitContext.Provider>
 }
+const cliente_id = "82d4ed29477f68045158"
+const cliente_secret = "412bc1b12514bd61b5a46df0d6aeddd993701510"
+
+const getUserInfos = async () => {
+  let data = await axios.get(`https://api.github.com/users/elvivx?client_id=${cliente_id}&client_secret=${cliente_secret}`)
+  console.log(data.data)
+}
+getUserInfos()
