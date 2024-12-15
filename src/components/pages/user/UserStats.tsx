c29f4652b132e6883d3ef6473854fc1aa204745e
Elvis <Elvisraphael00@gmail.com>
2024-12-15 23:49:15 +0100

    removed <hr/>

--- src/components/pages/user/UserStats.tsx
+++ src/components/pages/user/UserStats.tsx
@@ -5,11 +5,11 @@ function UserStats({ name }) {
       <div>
         <img src={`https://github-readme-stats.vercel.app/api?username=${name}&show_icons=true&theme=radical`} width='100%' alt='GitHub Stats' />
       </div>
-      <hr />
+
       <div>
         <img src={`https://github-readme-streak-stats.herokuapp.com/?user=${name}&theme=radical`} alt='GitHub Streak' width='100%' />
       </div>
-      <hr />
+
       <div>
         <img src={`https://github-readme-stats.vercel.app/api/top-langs/?username=${name}&langs_count=6&layout=compact&theme=radical`} width='100%' />
       </div>
