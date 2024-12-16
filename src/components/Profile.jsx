86550b6803769cd81cd2f015b2e14d86dcb7e723
Elvis <Elvisraphael00@gmail.com>
2024-12-17 00:44:28 +0100

    prevent loading empty profile

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,5 +1,18 @@
+import { useContext, useEffect } from "react"
 import User from "./pages/user/User"
+import { GitContext } from "./context/contexts"
+import { useNavigate } from "react-router"
+
 function Profile() {
+  const {
+    state: { user },
+  } = useContext(GitContext)
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if (user.length == 0) navigate(`/`)
+  }, [])
+
   return <User />
 }
 export default Profile
