fc54a9f1050ba296d6d8a27b71ca18bcc8931c7d
Elvis <Elvisraphael00@gmail.com>
2024-12-17 00:09:27 +0100

    nice animations

--- src/App.jsx
+++ src/App.jsx
@@ -49,6 +49,7 @@ function AppRoutes() {
     initial: { opacity: 0, x: 100 },
     animate: { opacity: 1, x: 0 },
     exit: { opacity: 0, x: -100 },
+    transition: { type: "spring", duration: 0.1 },
   }
 
   return (
