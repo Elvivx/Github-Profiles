f5aabbf8b06e19560cc17d40c98067a0b1232b3f
Elvis <Elvisraphael00@gmail.com>
2024-12-17 01:18:10 +0100

    removed rel in anchor

--- src/components/pages/user/UserItem.jsx
+++ src/components/pages/user/UserItem.jsx
@@ -38,7 +38,7 @@ function UserItem() {
             <img src={user.avatar_url} alt={user.login} />
           </div>
           <h1>
-            <a href={user.html_url} target='_blank' rel='noopener noreferrer'>
+            <a href={user.html_url} target='_blank'>
               {user.name || "N/A"} {<LinkIcon />}
             </a>
           </h1>
