95d5de8f0c49c6e733639be8aa250e00c26411ef
Elvis <Elvisraphael00@gmail.com>
2024-12-18 21:40:23 +0100

    recents styled again

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,14 +11,16 @@ function Home() {
   } = useContext(GitContext)
 
   const appTheme = useDeviceTheme()
+  console.log(appTheme)
   const app = useRef(document.querySelector("body"))
+
   useEffect(() => {
-    // if (theme !== "") return
     dispatch({ type: "theme", payload: appTheme })
     app.current.classList.add(appTheme)
-  }, [])
+  }, [appTheme])
 
   const themeMode = () => {
+    console.warn("shit")
     if (theme === "light") {
       app.current.classList.replace("dark", "light")
       dispatch({ type: "theme", payload: "dark" })
@@ -51,7 +53,7 @@ function Home() {
           <h1>Finder</h1>
         </div>
         <div className='theme' onClick={themeMode}>
-          {theme == "dark" && (
+          {theme === "light" && (
             <svg viewBox='0 0 256 256' xmlns='http://www.w3.org/2000/svg'>
               <rect fill='none' />
               <circle cx='128' cy='128' r='68' />
@@ -66,7 +68,7 @@ function Home() {
             </svg>
           )}
 
-          {theme == "light" && (
+          {theme === "dark" && (
             <svg viewBox='0 0 512 512' xmlns='http://www.w3.org/2000/svg'>
               <title />
               <path d='M264,480A232,232,0,0,1,32,248C32,154,86,69.72,169.61,33.33a16,16,0,0,1,21.06,21.06C181.07,76.43,176,104.66,176,136c0,110.28,89.72,200,200,200,31.34,0,59.57-5.07,81.61-14.67a16,16,0,0,1,21.06,21.06C442.28,426,358,480,264,480Z' />
